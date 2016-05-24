313cd46e2573afdb3254ffd21f0838c55d279de8
Robin Hernandez <robinsound@gmail.com>
2016-05-24 13:50:29 -0500

    Closes #1 "Implementar Tema Responsive"

---
+++ app/assets/javascripts/theme.js
@@ -0,0 +1,15 @@
+//= require ./theme/jquery.min.js
+//= require ./theme/bootstrap.min.js
+//= require ./theme/jquery.flot.js
+//= require ./theme/jquery.flot.resize.js
+//= require ./theme/curvedLines.js
+//= require ./theme/jquery.sparkline.min.js
+//= require ./theme/jquery.easypiechart.min.js
+//= require ./theme/moment.min.js
+//= require ./theme/fullcalendar.min.js
+//= require ./theme/jquery.simpleWeather.min.js
+//= require ./theme/waves.min.js
+//= require ./theme/bootstrap-growl.min.js
+//= require ./theme//sweetalert.min.js
+//= require ./theme/jquery.mCustomScrollbar.concat.min.js
+//= require ./theme/app.js
